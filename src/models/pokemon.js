1d7bea16a6bc399b806d1e62c87989326e3c9b0b
Julien <mmos002@gmail.com>
2022-03-30 10:15:14 +0200

    Wed push

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -1,29 +1,61 @@
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define(
+return sequelize.define(
     "Pokemon",
     {
-      id: {
+    id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
-      },
-      name: {
+    },
+    name: {
         type: DataTypes.STRING,
         allowNull: false,
-      },
-      hp: {
+        validate: {
+            notEmpty: { msg: `Veuillez ajouter un nom à votre pokémon.`}, 
+            notNull: { msg: `Le nom est une proprité requise.`}
+        }
+    },
+    hp: {
         type: DataTypes.INTEGER,
         allowNull: false,
-      },
-      cp: {
+        validate: {
+            min: {
+                args: [0], 
+                msg: `Veuillez ajouter une valeur supérieure à 0.`
+            },
+            max: {
+                args: [1000],
+                msg: `La valeur doit être inférieure à 1000.`
+            },
+            isInt: { msg: `Utilisez uniquement des nombres entiers pour les points de vie.`},
+            notNull: { msg: `Les points de vie sont une proprité requise.`}
+        }
+    },
+    cp: {
         type: DataTypes.INTEGER,
         allowNull: false,
-      },
-      picture: {
+        validate: {
+            min: {
+                args: [0],
+                msg: `Veuillez ajouter une valeur supérieure à 0.`
+            },
+            max: {
+                args: [100],
+                msg: `Les dégàts ne peuvent pas dépasser 100.`
+            },
+            isInt: { msg: `Utilisez uniquement des nombres entiers pour les dégàts.`},
+            notNull: { msg: `Les dégàts sont une proprité requise.`}
+        }
+    },
+    picture: {
         type: DataTypes.STRING,
         allowNull: false,
-      },
-      types: {
+        validate: {
+            isUrl: { msg: `Veuillez indiquer une URL valide pour l'image.`},
+            notNull: { msg: `L'image est une propriété requise.`}
+        }
+    },
+    types: {
         type: DataTypes.STRING,
         allowNull: false,
         get() {
@@ -32,12 +64,12 @@ module.exports = (sequelize, DataTypes) => {
         set(types) {
             this.setDataValue('types', types.join())
         }
-      }
+    }
     },
     {
-      timestamps: true,
-      createdAt: "created",
-      updatedAt: false,
+    timestamps: true,
+    createdAt: "created",
+    updatedAt: false,
     }
-  );
+);
 };
